9b67642f86e9a5b8ac0590dde3cf6dc91d134b68
vatsalSabalpara18 <vatsalsabalpara18@gmail.com>
2025-01-13 09:42:26 +0530

    Refactor SubCategory management to use async actions with Axios and update state structure

--- src/redux/reducer/slice/subcategory.slice.js
+++ src/redux/reducer/slice/subcategory.slice.js
@@ -1,36 +1,105 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
+
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import axios from "axios"
+
+// const initialState = {
+//     isLoading: false,
+//     subcategories: [],
+//     error: null,
+// };
+
+// const subcategorySlice = createSlice({
+//     name: "subcategory",
+//     initialState,
+//     reducers: {
+//         getSubCategories: (state, action) => { },
+//         addSubCategories: (state, action) => {
+//             state.subcategories.push(action.payload);
+//         },
+//         deleteCategories: (state, action) => {
+//             state.subcategories = state.subcategories.filter(
+//                 (v) => v.id !== action.payload
+//             );
+//         },
+//         updateCategories: (state, action) => {
+//             state.subcategories = state.subcategories.map((v) =>
+//                 v.id === action.payload.id ? action.payload : v
+//             );
+//         },
+//     },
+// });
+
+// export const {
+//     addSubCategories,
+//     getSubCategories,
+//     deleteCategories,
+//     updateCategories,
+// } = subcategorySlice.actions;
+// export default subcategorySlice.reducer;
 
 const initialState = {
     isLoading: false,
-    subcategories: [],
-    error: null,
-};
+    subCategoryData: [],
+    error: null
+}
 
-const subcategorySlice = createSlice({
-    name: "subcategory",
+
+export const getSubCategories = createAsyncThunk(
+    'subCategory/getSubCategories',
+    async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/subcategory')
+            console.log(response.data)
+            return response.data
+        } catch (error) {
+            console.error(error)
+        }
+    }
+)
+
+export const addSubCategories = createAsyncThunk(
+    'subCategory/addSubCategories',
+    async (data) => {
+        try {
+            const response = await axios.post("http://localhost:5000/subcategory", data)
+            console.log(response.data);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+)
+export const deleteCategories = createAsyncThunk(
+
+)
+export const updateCategories = createAsyncThunk(
+    "subCategory/updateCategories",
+    async (data) => {
+        try {            
+            const response = await axios.put(`http://localhost:5000/subcategory/${data?.id}`, data)
+            console.log(response.data)
+            // return response.data
+        } catch (error) {
+            console.error(error)
+        }
+    }
+)
+
+
+const subCategorySlice = createSlice({
+    name: 'subCategory',
     initialState,
-    reducers: {
-        getSubCategories: (state, action) => { },
-        addSubCategories: (state, action) => {
-            state.subcategories.push(action.payload);
-        },
-        deleteCategories: (state, action) => {
-            state.subcategories = state.subcategories.filter(
-                (v) => v.id !== action.payload
-            );
-        },
-        updateCategories: (state, action) => {
-            state.subcategories = state.subcategories.map((v) =>
-                v.id === action.payload.id ? action.payload : v
-            );
-        },
-    },
-});
+    extraReducers: (builder) => {
+        builder.addCase(getSubCategories.fulfilled, (state, action) => {
+            state.subCategoryData = action.payload
+        })
+        builder.addCase(addSubCategories.fulfilled, (state, action) => {
+            state.subCategoryData.push(action.payload)
+        })
+        builder.addCase(updateCategories.fulfilled, (state, action) => {
+            state.subCategoryData = state.subCategoryData.map((v) => v.id === action.payload.id ? action.payload : v);
+        })
+    }
+})
 
-export const {
-    addSubCategories,
-    getSubCategories,
-    deleteCategories,
-    updateCategories,
-} = subcategorySlice.actions;
-export default subcategorySlice.reducer;
+export default subCategorySlice.reducer
