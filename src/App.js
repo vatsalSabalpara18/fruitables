6b554e7e81d05d2631446421aee417090b0691f7
vatsalSabalpara18 <vatsalsabalpara18@gmail.com>
2025-01-16 08:37:42 +0530

    Add redux-persist for state persistence and update store configuration

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import UserRoutes from "./routes/UserRoutes";
 import PrivateRoutes from "./routes/PrivateRoutes";
 import AdminRoutes from "./routes/AdminRoutes";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { createStore } from "./redux/store";
 
 function App() {
-  const store = createStore();
+  const {store, persistor} = createStore();
   return (
     <>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <Routes>
         <Route path="/*" element={<UserRoutes />} />
         <Route element={<PrivateRoutes />} >
           <Route path="/admin/*" element={<AdminRoutes />} />
         </Route>
       </Routes>
+        </PersistGate>
     </Provider>
     </>
   );
