62e80107188609871852102d78726c3ed198f2e3
vatsalSabalpara18 <vatsalsabalpara18@gmail.com>
2025-04-26 08:21:04 +0530

    init setup

--- src/utills/baseURL.js
+++ src/utills/baseURL.js
@@ -1,5 +1,8 @@
-export const API_BASE_URL = "http://localhost:8081/api/v1/"
+export const API_BASE_URL = "https://fruitables-backend.vercel.app/api/v1/"
 
-export const IMAGE_URL = "http://localhost:8081/"
+export const IMAGE_URL = "https://fruitables-backend.vercel.app/"
+// export const API_BASE_URL = "http://localhost:8081/api/v1/"
+
+// export const IMAGE_URL = "http://localhost:8081/"
 
 export const CHAT_URL = "http://localhost:8080/"
